1ff5021f6f8c11c6920d481f296072a5a87d8467
Timur Kashapov <graphikov@gmail.com>
2016-10-17 18:19:46 +0300

    init commit

--- src/main/java/NeuralNetwork.java
+++ src/main/java/NeuralNetwork.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
+
 /**
  * @author Timur Kashapov
  * @since 17.10.2016
  */
 public class NeuralNetwork {
+
+    private int countOfNeurons;
+
+    private Neuron neuron;
+    private int getCountOfNeuronsInNeuralNetwork;
+
+    private InputLayer inputLayer;
+    private OutputLayer outputLayer;
+
+    private ArrayList<HiddenLayer > hiddenLayerArrayList;
+    private int numberOfHiddenLayers;
+
+
+
+    public void initNet() {
+
+    }
+
+    public void printNet() {
+
+    }
+
 }
